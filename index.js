16e54ad20fe4b35cde3cb352c6bbd3be3fd93b4d
Kaspars Liepins <kasparslie@gmail.com>
2020-10-04 13:09:34 +0200

    innitial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var newVar = require ("./information")
+ console.log (newVar);
+
+
+ var cowsay = require("cowsay");
+
+ console.log(cowsay.say({
+	 text : "I'm a moooodule",
+	 e : "oO",
+	 T : "U "
+ }));
+ 
+ 
---
+++ information.js
@@ -0,0 +1,6 @@
+const infoE = {
+name: "Kaspars",
+campus: "Amsterdam",
+};
+
+module.exports = infoE;
